96f509ac0b61fda2517eaa642682ffe810904562
Clyde Shaffer <clydeshaffer@gmail.com>
2020-06-22 02:41:57 -0400

    Emulate gamepad input from keyboard

--- gtc.cpp
+++ gtc.cpp
@@ -65,6 +65,11 @@ SDL_Surface* vRAM_Surface = NULL;
 
 mos6502 *cpu_core;
 
+bool pad1State = false;
+bool pad2State = false;
+uint16_t pad1Mask = 0;
+uint16_t pad2Mask = 0;
+
 uint8_t open_bus() {
 	return 0;
 }
@@ -226,6 +231,20 @@ uint8_t MemoryRead(uint16_t address) {
 		return VDMA_Read(address);
 	} else if(address < 0x2000) {
 		return ram_buffer[address & 0x1FFF];
+	} else if(address == 0x2008) {
+		pad2State = false;
+		uint8_t outbyte = 0xFF;
+		if(pad1State) {
+			outbyte = (uint8_t) (pad1Mask >> 8);
+		} else {
+			outbyte = (uint8_t) pad1Mask;
+		}
+		pad1State = !pad1State;
+		return ~outbyte;
+	} else if(address == 0x2009) {
+		pad1State = false;
+		pad2State = !pad2State;
+		return 0xFF;
 	}
 	return open_bus();
 }
@@ -317,7 +336,7 @@ int main(int argC, char* argV[]) {
 	uint64_t cycles_since_vsync = 0;
 	uint64_t cycles_per_vsync = 233333;
 	uint64_t target_runtime = 60;
-	uint64_t target_cycles = target_runtime * 14000000;
+	uint64_t target_cycles = target_runtime * 3000000;
 	int zeroConsec = 0;
 	while(running) {
 		actual_cycles = 0;
@@ -331,7 +350,7 @@ int main(int argC, char* argV[]) {
 		} else {
 			zeroConsec = 0;
 		}
-		SDL_Delay(actual_cycles/140000);
+		SDL_Delay(actual_cycles/3000000);
 		cycles_since_vsync += actual_cycles;
 		if(cycles_since_vsync >= cycles_per_vsync) {
 			cycles_since_vsync -= cycles_per_vsync;
@@ -347,6 +366,52 @@ int main(int argC, char* argV[]) {
             if( e.type == SDL_QUIT )
             {
                running = false;
+            } else if(e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
+            	uint16_t buttonMask = 0;
+            	/*
+            		Up - DB9 pin 1 - bit 3
+            		Down - DB9 pin 2 - bit 2
+            		Left - DB9 pin 3 (select HIGH) - bit 1
+            		Right - DB9 pin 4 (select HIGH) - bit 0
+            		A - DB9 pin 6 (select LOW) - bit 4
+            		B - DB9 pin 6 (select HIGH) - bit 4
+            		C - DB9 pin 9 (select HIGH) - bit 5
+            		Start - DB9 pin 9 (select LOW) - bit 5
+            		select status - bit 7
+            	*/
+            	switch(e.key.keysym.sym) {
+            		case SDLK_UP:
+            			buttonMask = 0b0000100000001000;
+            			break;
+            		case SDLK_DOWN:
+            			buttonMask = 0b0000010000000100;
+            			break;
+            		case SDLK_LEFT:
+            			buttonMask = 0b0000001000000000;
+            			break;
+            		case SDLK_RIGHT:
+            			buttonMask = 0b0000000100000000;
+            			break;
+            		case SDLK_z:
+            			buttonMask = 0b0000000000010000;
+            			break;
+            		case SDLK_x:
+            			buttonMask = 0b0001000000000000;
+            			break;
+            		case SDLK_c:
+            			buttonMask = 0b0010000000000000;
+            			break;
+            		case SDLK_RETURN:
+            			buttonMask = 0b0000000000100000;
+            			break;
+            		default:
+            			break;
+            	}
+        		if(e.type == SDL_KEYDOWN) {
+        			pad1Mask |= buttonMask;
+        		} else {
+        			pad1Mask &= ~buttonMask;
+        		}
             }
         }
 		
